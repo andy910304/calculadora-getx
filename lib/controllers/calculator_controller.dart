81bc7842fb78aeef8d5a541c5f363fa7b519f202
Andy <andy910304@gmail.com>
2021-07-03 23:08:34 -0400

    first part calculator controller

---
+++ lib/controllers/calculator_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class CalculatorController extends GetxController {
+  var firstNumber = '10'.obs;
+  var secondNumber = '20';
+  var mathResult = '30';
+  var operation = '+';
+}
